e89ffbb61fba733954af738813c0a2a8ecc9810e
DanLuchi <dluchi@gmail.com>
2013-11-18 15:50:25 -0500

    refactor MoodAverage.happiness since method

--- app/models/mood_average.rb
+++ app/models/mood_average.rb
@@ -1,13 +1,8 @@
 class MoodAverage
   def self.happiness_since(date)
-    total_happiness = 0
     moods = Mood.since(date)
-
-    moods.each do |mood|
-      total_happiness += 1 if mood.happy?
-    end
-
     return 0 if moods.count == 0
-    total_happiness.to_f / moods.count
+
+    moods.where(happy: true).count.to_f / moods.count
   end
 end
