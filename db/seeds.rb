8b9dc7b574df44d11c3899aca3757eaa107da47d
Jeremy Weiskotten <jeremy@teamlaunch.com>
2016-03-28 14:14:42 -0400

    Seed with 100 random moods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do
+  Mood.create!(happy: [true, false].sample, created_at: rand(14).days.ago)
+end
